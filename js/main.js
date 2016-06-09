d044a11f19b5139cafc80751a99d00d89faa8640
Insane <insanepl@gmail.com>
2016-06-09 22:17:58 +0200

    Homepage Finished

--- js/main.js
+++ js/main.js
@@ -25,11 +25,41 @@ $(window).load(function() {
     TweenMax.set(page, {backgroundColor: '#000'});
     TweenMax.set(pageContent, {opacity: '1'});
     TweenLite.to(revealContainer, 1, {opacity:'0', ease: Power3.easeOut, onComplete: removeFromDom});
-
   }
 
   /* Remove Unused Elements from DOM */
   function removeFromDom() {
     revealContainer.remove();
+    TweenMax.set(page, {overflow: 'auto'});
   }
 });
+
+/* Burger Menu Trigger */
+var clicked = false;
+var menuContainer = $('.menu-reveal');
+var menuItem = $('.menu-item');
+TweenLite.set(menuItem, { y: '-100%', opacity: '0' });
+var tl = new TimelineLite();
+
+$('.burger-menu').on('click', function() {
+  if(clicked){
+    $(this).removeClass('is-active');
+    TweenLite.to(menuContainer, 1, {opacity:'0', ease: Power3.easeOut, onComplete: setDefaults});
+
+    function setDefaults() {
+      TweenMax.set(menuContainer, {visibility: 'hidden', pointerEvents : 'none'});
+      TweenLite.set(menuItem, { y: '-100%', opacity: '0' });
+    }
+  } else {
+    $(this).addClass('is-active');
+    TweenMax.set(menuContainer, {visibility: 'initial', pointerEvents : 'all'});
+    TweenLite.to(menuContainer, .6, {opacity:'1', ease: Power3.easeOut, onComplete: animateItems});
+
+    function animateItems() {
+      tl.staggerTo(menuItem, .6, { y:'1%', opacity:'1', ease:Back.easeOut }, 0.2);
+    }
+  }
+  clicked = !clicked; /* Toggle Clicked */
+});
+
+/* Move Video */
